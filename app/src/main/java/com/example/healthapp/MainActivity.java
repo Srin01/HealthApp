023f286b2fbc34253e05b9fec008dd764862aa39
Srinidhi S Prasad <64426998+Srin01@users.noreply.github.com>
2020-10-18 02:50:49 +0530

    Second commit

--- app/src/main/java/com/example/healthapp/MainActivity.java
+++ app/src/main/java/com/example/healthapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageViewHeader;
+    ImageView bmiImage;
+    ImageView waterIndex;
+    ImageView calorieIndex;
+    ImageView distance;
+
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
+    }
+
+    private void bindViews() {
+        imageViewHeader = findViewById(R.id.imageView_header);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        bmiImage = findViewById(R.id.profile_image1);
+        waterIndex = findViewById(R.id.profile_image2);
+        calorieIndex = findViewById(R.id.profile_image3);
+        distance = findViewById(R.id.profile_image4);
     }
 }
